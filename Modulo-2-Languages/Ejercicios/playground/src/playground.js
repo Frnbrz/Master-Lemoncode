09aadad09479df10e9053d8bb5117f31cfbbd05c
frnbrz <frnbrz@gmail.com>
2024-12-08 23:22:38 +0100

    refactor: JSON files for consistent formatting and indentation; improve readability in playground.js

--- Modulo-2-Languages/Ejercicios/playground/src/playground.js
+++ Modulo-2-Languages/Ejercicios/playground/src/playground.js
@@ -1,30 +1,28 @@
 // Descifra el siguiente secreto:
 var secret =
-  "': rg!qg yq,urae: ghsrf wuran shrerg jq,u'qf ra r' ,qaq' er g'q,o rg,fuwurae: m!hfua( t'usqfuq ,:apu(:m xv"
+	"': rg!qg yq,urae: ghsrf wuran shrerg jq,u'qf ra r' ,qaq' er g'q,o rg,fuwurae: m!hfua( t'usqfuq ,:apu(:m xv"
 
 // Sabiendo que el alfabeto original ha sufrido la siguiente transformación:
 var plain = "abcdefghijklmnopqrstuvwxyz:()!¡,'"
 var cipher = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm"
 
-function decrypt (secret) {
-  // replace cipher with plain " " are the same 
-  var decrypted = ""
+function decrypt(secret) {
+	// replace cipher with plain " " are the same
+	var decrypted = ""
 
-  for (var i = 0; i < secret.length; i++) {
-    if (secret[i] === " ") {
-      decrypted += " "
-    } else {
-      for (var j = 0; j < cipher.length; j++) {
-        if (secret[i] === cipher[j]) {
-          decrypted += plain[j]
-        }
-      }
-    }
-  }
+	for (var i = 0; i < secret.length; i++) {
+		if (secret[i] === " ") {
+			decrypted += " "
+		} else {
+			for (var j = 0; j < cipher.length; j++) {
+				if (secret[i] === cipher[j]) {
+					decrypted += plain[j]
+				}
+			}
+		}
+	}
 
-
-  return decrypted
+	return decrypted
 }
-
 
 console.log(decrypt(secret))
