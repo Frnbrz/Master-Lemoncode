5ec76dd9bb7a4a093be57a5075b06240a9383e56
frnbrz <frnbrz@gmail.com>
2025-01-14 22:57:42 +0100

    fix: add additional console logs for NaN checks in understanding-js.js

--- Modulo-2-Languages/Retos/understanding-js.js
+++ Modulo-2-Languages/Retos/understanding-js.js
@@ -30,3 +30,12 @@ console.log(!isNaNValue(x) && x !== x); // false
 console.log(!isNaNValue(x) && x !== x); // false
 
 console.log(x > x); // true
+
+
+
+console.log(!isNaNValue(x) && x !== x); // false
+
+
+console.log(!isNaNValue(x) && x !== x); // false
+
+console.log(x > x); // true
