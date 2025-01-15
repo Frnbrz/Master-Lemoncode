764b361b64dc18b3814bfd2f065a95cf4e94f5a5
frnbrz <frnbrz@gmail.com>
2025-01-15 23:28:33 +0100

    fix: remove redundant console logs in understanding-js.js

--- Modulo-2-Languages/Retos/understanding-js.js
+++ Modulo-2-Languages/Retos/understanding-js.js
@@ -8,34 +8,3 @@ console.log(x === x); // false
 console.log(!isNaNValue(x) && x !== x); // false
 
 
-console.log(!isNaNValue(x) && x !== x); // false
-
-console.log(x > x); // true
-
-
-
-
-console.log(!isNaNValue(x) && x !== x); // false
-
-
-console.log(!isNaNValue(x) && x !== x); // false
-
-console.log(x > x); // true
-
-
-
-console.log(!isNaNValue(x) && x !== x); // false
-
-
-console.log(!isNaNValue(x) && x !== x); // false
-
-console.log(x > x); // true
-
-
-
-console.log(!isNaNValue(x) && x !== x); // false
-
-
-console.log(!isNaNValue(x) && x !== x); // false
-
-console.log(x > x); // true
