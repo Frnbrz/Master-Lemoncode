980391667abedcbeb782d2c329ceb9c9386f9622
frnbrz <frnbrz@gmail.com>
2025-01-17 14:35:46 +0100

    feat: add MemberTableHeader component for table headers

---
+++ Modulo-4-Frameworks/01-React/Ejercicios/list-exercise/src/components/MemberTableHeader.tsx
@@ -0,0 +1,10 @@
+export default function MemberTableHeader() {
+  return (
+    <>
+      <span className="header">Avatar</span>
+      <span className="header">Id</span>
+      <span className="header">Name</span>
+      <span className="header">Github</span>
+    </>
+  )
+}
