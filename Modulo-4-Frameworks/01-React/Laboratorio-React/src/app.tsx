03e5abe39e714b7da5c7e6cec6a32f78702e2593
frnbrz <frnbrz@gmail.com>
2025-02-15 20:56:12 +0100

    feat: remove AuthProvider wrapper from App component

--- Modulo-4-Frameworks/01-React/Laboratorio-React/src/app.tsx
+++ Modulo-4-Frameworks/01-React/Laboratorio-React/src/app.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { AuthProvider } from "./context/user/user.context";
 import AppRoutes from "./router";
 
 export const App = () => {
   return (
-    <AuthProvider>
-      <AppRoutes />
-    </AuthProvider>
+    <AppRoutes />
   );
 };
