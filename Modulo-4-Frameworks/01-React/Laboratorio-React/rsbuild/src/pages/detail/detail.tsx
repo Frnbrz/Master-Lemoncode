97c494b2c2863e94e858ae799bc47c362a982ac4
frnbrz <frnbrz@gmail.com>
2025-02-19 20:48:35 +0100

    refactor: remove header from DetailPage component

--- Modulo-4-Frameworks/01-React/Laboratorio-React/rsbuild/src/pages/detail/detail.tsx
+++ Modulo-4-Frameworks/01-React/Laboratorio-React/rsbuild/src/pages/detail/detail.tsx
@@ -6,9 +6,6 @@ const DetailPage: React.FC = () => {
 
   return (
     <>
-      <header>
-        <h2>Hello from Detail page</h2>
-      </header>
       <h3>User Id: {id}</h3>
       <Link to="/list">Back to list page</Link>
     </>
