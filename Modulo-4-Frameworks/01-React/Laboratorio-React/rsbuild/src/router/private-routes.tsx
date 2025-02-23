67accb2cae766b74a92a5216cec96e1e80f7edf1
frnbrz <frnbrz@gmail.com>
2025-02-24 00:11:28 +0100

    feat: enhance Layout component to display dynamic page context in greeting

--- Modulo-4-Frameworks/01-React/Laboratorio-React/rsbuild/src/router/private-routes.tsx
+++ Modulo-4-Frameworks/01-React/Laboratorio-React/rsbuild/src/router/private-routes.tsx
@@ -2,11 +2,14 @@ import React, { lazy, Suspense } from "react";
 import { AuthGuard } from "./router.guard";
 import { PrivateRoutesTypes } from "./router.types";
 import { useAuth } from "src/context/user/user.context";
+import { useLocation } from "react-router-dom";
 
 const ListPage = lazy(() => import("../pages/list/list"));
 const DetailPage = lazy(() => import("../pages/detail/detail"));
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { logout, user } = useAuth()
+  const { pathname } = useLocation()
+  const getUrlFistParam = pathname.split('/')[1]
   const handleLogout = () => {
     logout()
   }
@@ -14,10 +17,11 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div>
       <header>
-        <h2>Hello {user} from List page</h2>
+        <h2>Hello {user} from {getUrlFistParam} page</h2>
         <button className="btn btn-sm" onClick={() => handleLogout()}>logout</button>
       </header>
-      {children}</div>
+      {children}
+    </div>
   )
 }
 
