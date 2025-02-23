2cf271d72701e58fd3a3030d3093eead61c1fdf8
frnbrz <frnbrz@gmail.com>
2025-02-24 00:11:20 +0100

    feat: update fetchMembers function to accept organization name as a parameter

--- Modulo-4-Frameworks/01-React/Laboratorio-React/rsbuild/src/api/memberAPI.ts
+++ Modulo-4-Frameworks/01-React/Laboratorio-React/rsbuild/src/api/memberAPI.ts
@@ -1,5 +1,6 @@
+import { MemberEntity } from "src/types/members";
 
-export const fetchMembers = async () => {
-  const response = await fetch(`https://api.github.com/orgs/lemoncode/members`);
+export async function fetchMembers(orgName: string): Promise<MemberEntity[]> {
+  const response = await fetch(`https://api.github.com/orgs/${orgName}/members`);
   return await response.json();
 };
