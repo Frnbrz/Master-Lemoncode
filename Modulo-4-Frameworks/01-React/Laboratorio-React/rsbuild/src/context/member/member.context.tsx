1d2c19d6808d511f17688167b0f3403ee2e80957
frnbrz <frnbrz@gmail.com>
2025-02-24 22:23:10 +0100

    feat: refactor MembersProvider to use fetchMembers function and add useMember hook

--- Modulo-4-Frameworks/01-React/Laboratorio-React/rsbuild/src/context/member/member.context.tsx
+++ Modulo-4-Frameworks/01-React/Laboratorio-React/rsbuild/src/context/member/member.context.tsx
@@ -1,44 +1,42 @@
-import { createContext, useState, useEffect, PropsWithChildren } from 'react';
+import { createContext, useState, useEffect, PropsWithChildren, useContext } from 'react';
+import { fetchMembers } from 'src/api/memberAPI';
 import { MemberEntity } from 'src/types/members';
 
 interface MembersContextType {
   members: MemberEntity[];
   loading: boolean;
   error: string | null;
+  setMembers: React.Dispatch<React.SetStateAction<MemberEntity[]>>;
 }
 
 export const MembersContext = createContext<MembersContextType>({
   members: [],
   loading: true,
   error: null,
+  setMembers: () => { },
 });
 
-export const GithubMembersProvider: React.FC<PropsWithChildren<{}>> = ({ children }) => {
-  const [members, setMembers] = useState([]);
+export const MembersProvider: React.FC<PropsWithChildren<{}>> = ({ children }) => {
+  const [members, setMembers] = useState<MemberEntity[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
+  console.log(error)
   useEffect(() => {
-    const fetchMembers = async () => {
-      try {
-        const response = await fetch('https://api.github.com/orgs/{organización}/members');
-        const data = await response.json();
-        setMembers(data);
-      } catch (err) {
-        if (err instanceof Error) {
-          setError(err.message)
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchMembers();
+    fetchMembers("lemoncode")
+      .then((members) => setMembers(members))
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false));
   }, []);
 
   return (
-    <MembersContext.Provider value={{ members, loading, error }}>
+    <MembersContext.Provider value={{ members, loading, error, setMembers }}>
       {children}
     </MembersContext.Provider>
   );
 };
+
+export const useMember = () => {
+  return useContext(MembersContext);
+};
+
