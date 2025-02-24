b19cc0736a953bcebf16d22cb5efbc19239c4561
frnbrz <frnbrz@gmail.com>
2025-02-24 22:23:19 +0100

    feat: enhance MemberFinder component to handle organization input and display error messages

--- Modulo-4-Frameworks/01-React/Laboratorio-React/rsbuild/src/pages/list/components/MemberFinder.tsx
+++ Modulo-4-Frameworks/01-React/Laboratorio-React/rsbuild/src/pages/list/components/MemberFinder.tsx
@@ -1,15 +1,29 @@
+import { fetchMembers } from "src/api/memberAPI"
+import { useMember } from "src/context/member/member.context";
+
 export default function MemberFinder() {
+  const { error } = useMember()
 
-  const handleGetMembers = () => {
 
+  const handleGetMembers = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const orgName = formData.get("orgName") as string;
+    fetchMembers(orgName);
   }
+
+
   return (
+
     <form onSubmit={handleGetMembers}>
-      <label>Username:
-        <input
-        />
-      </label>
+      <p>
+        <label htmlFor="orgName">Organización:</label>
+        <input type="text" name="orgName" id="orgName" placeholder="lemoncode" />
+      </p>
       <button type="submit" className="btn btn-sm smooth">Login</button>
-    </form>
+
+      {error && <p className="error-text">{error}</p>}
+    </form >
+
   )
 }
